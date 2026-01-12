e2f49d2b5424088a459bed5867b01fe7c47952ee
CoCodeStudio <cocodestudio.org@gmail.com>
2026-01-12 11:49:53 +0530

    Nullable Changes

--- NotifyX/src/main/java/com/cocode/notifyx/model/MessageType.java
+++ NotifyX/src/main/java/com/cocode/notifyx/model/MessageType.java
@@ -1,11 +1,11 @@
 package com.cocode.notifyx.model;
 
 public enum MessageType {
-    APP_UPDATE,
-    INFORMATIONAL,
-    SUCCESS,
-    ERROR,
-    WARNING;
+    APP_UPDATE, // For DIALOGS
+    INFORMATIONAL, // For DIALOGS & TOAST
+    SUCCESS, // For TOAST
+    ERROR, // For TOAST
+    WARNING; // For DIALOGS & TOAST
 
     public static MessageType fromString(String s) {
         if (s == null) return INFORMATIONAL;
